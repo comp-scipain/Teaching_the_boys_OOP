a4dc82e3c7a31353c4dc2c0c850910bdc60a7ca2
comp-scipain <72593254+comp-scipain@users.noreply.github.com>
2021-12-15 15:30:43 -0800

    Add files via upload
    You can now create another bitch.

--- src/Beat.java
+++ src/Beat.java
@@ -40,13 +40,54 @@ public class Beat extends Bitch {
             }
         }
 
-        System.out.println("The bitch you created:");
+        System.out.println(" ");
+        System.out.println("Create another bitch.");
+        System.out.println(" ");
+        System.out.println("What is their name?");
+        name = keyboard.next();
+        bitch2.setName(name);
+        System.out.println("How old are they?");
+        age = keyboard.nextInt();
+        bitch2.setAge(age);
+        System.out.println("Do you like traps? Answer yes or no");
+        answer = keyboard.next();
 
+        System.out.println(" ");
+        answered = false;
+        while (!answered) {
+            if (answer.equalsIgnoreCase(option1)) {
+                boolean AreFemale = false;
+                bitch2.setGender(AreFemale);
+                answered = true;
+            } else if (answer.equalsIgnoreCase(option2)) {
+                boolean AreFemale2 = true;
+                bitch2.setGender(AreFemale2);
+                answered = true;
+            } else {
+                System.out.println("That is not an option. It is a yes or no question");
+                answer = keyboard.next();
+            }
+        }
+
+        System.out.println("The bitches you created:");
+        System.out.println("This is the first bitch you created.");
         System.out.println("Their name is " + bitch1.getName() + ".");
         System.out.println("They are " + bitch1.getAge() + " years old.");
         if (bitch1.getGender()) {
             System.out.println("You are straight my niggalo");
         } else if (!bitch1.getGender()) {
+            System.out.println("You are gay. lmao");
+        } else {
+            System.out.println("I don't know how you are seeing this");
+        }
+
+        System.out.println(" ");
+        System.out.println("This is the second bitch you created");
+        System.out.println("Their name is " + bitch2.getName() + ".");
+        System.out.println("They are " + bitch2.getAge() + " years old.");
+        if (bitch2.getGender()) {
+            System.out.println("You are straight my niggalo");
+        } else if (!bitch2.getGender()) {
             System.out.println("You are gay. lmao");
         } else {
             System.out.println("I don't know how you are seeing this");
